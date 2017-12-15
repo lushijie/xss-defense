0e86bcbe2322cfb9faefbed03cbe24e9e2d45a26
lushijie <lushijie1218@126.com>
2017-12-15 17:13:11 +0800

    jquery.encoder.js

--- jquery.encoder.js
+++ jquery.encoder.js
@@ -1,22 +1,67 @@
-/*!
- * Simple JavaScript Inheritance
- * By John Resig http://ejohn.org/
- * MIT Licensed.
- */
-(function(a){var c=false,e=/xyz/.test(function(){xyz;})?/\b_super\b/:/.*/;
-function b(){}function d(){var m=Array.prototype.slice.call(arguments);var l=0;var k=m.length;var h=new this();var n,o;for(;l<k;l++){n=m[l];for(o in n){h[o]=n[o];
-}}function g(){}g.prototype=h;g.prototype.constructor=g;g.inject=arguments.callee;g.extend=f;return g;}function f(j){var i=this.prototype;c=true;var h=new this();
-c=false;for(var k in j){h[k]=typeof j[k]=="function"&&typeof i[k]=="function"&&e.test(j[k])?(function(m,l){return function(){var o=this._super;this._super=i[m];
-var n=l.apply(this,arguments);this._super=o;return n;};})(k,j[k]):j[k];}function g(){if(!c&&this.init){this.init.apply(this,arguments);}}g.prototype=h;
-g.prototype.constructor=g;g.extend=arguments.callee;g.inject=d;return g;}b.inject=d;b.extend=f;b.version="1.1.4";if(typeof define!=="undefined"){return define(function(h,g,i){i.exports=b;
-});}if(typeof exports==="undefined"){a.Class=b;return;}module.exports=b;})(this);
-
-
-
 /*! jQuery Encoder Plugin - v0.1.1 - 2015-08-19
 * https://github.com/chrisisbeef/jquery-encoder
 * Copyright (c) 2015 Chris Schmidt; Licensed MIT */
 (function($) {
+
+  // https://gist.github.com/jhbsk/4380541#file-class-js
+  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+    // The base Class implementation (does nothing)
+    this.Class = function(){};
+
+    // Create a new Class that inherits from this class
+    Class.extend = function(prop) {
+      var _super = this.prototype;
+
+      // Instantiate a base class (but only create the instance,
+      // don't run the init constructor)
+      initializing = true;
+      var prototype = new this();
+      initializing = false;
+
+      // Copy the properties over onto the new prototype
+      for (var name in prop) {
+        // Check if we're overwriting an existing function
+        prototype[name] = typeof prop[name] == "function" &&
+          typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+          (function(name, fn){
+            return function() {
+              var tmp = this._super;
+
+              // Add a new ._super() method that is the same method
+              // but on the super-class
+              this._super = _super[name];
+
+              // The method only need to be bound temporarily, so we
+              // remove it when we're done executing
+              var ret = fn.apply(this, arguments);
+              this._super = tmp;
+
+              return ret;
+            };
+          })(name, prop[name]) :
+          prop[name];
+      }
+
+      // The dummy class constructor
+      function Class() {
+        // All construction is actually done in the init method
+        if ( !initializing && this.init )
+          this.init.apply(this, arguments);
+      }
+
+      // Populate our constructed prototype object
+      Class.prototype = prototype;
+
+      // Enforce the constructor to be what we expect
+      Class.prototype.constructor = Class;
+
+      // And make this class extendable
+      Class.extend = arguments.callee;
+
+      return Class;
+    };
+
+  // begin encoder
   var default_immune = {
     'js'        : [',','.','_',' ']
   };
